ecdc2369c4ac5cd074dc0293aed073266413f71a
Sumit Patel <sumitpatel2907@gmail.com>
2025-08-17 01:11:43 +0530

    Trying the deployment again

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
-  base:'./'
+  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 3000,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173,
+  }
 })
