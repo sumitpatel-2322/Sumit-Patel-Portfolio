c7fc34ed1b5974dfbc24646f95b7d59f61e0bee8
Sumit Patel <sumitpatel2907@gmail.com>
2025-08-17 01:21:26 +0530

    Deployment try:3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -11,9 +12,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 3000,
+    allowedHosts: ['sumit-patel-portfolio.onrender.com']
   },
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: ['sumit-patel-portfolio.onrender.com']
   }
 })
