4f7f8f996566d42223d0a7a60a65059faf26a89d
Sumit Patel <sumitpatel2907@gmail.com>
2025-09-16 01:39:17 +0530

    Fixed the bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import About from './components/About';
 import ProjectCard from './components/ProjectCard';
 import portfolioimg from './assets/Portfolio.jpeg';
 import rankmateimg from './assets/Rankmate.jpeg';
+import Clusterimg from './assets/Cluster_bar_graph.jpeg';
+import Tradingbotimg from './assets/Trading_bot.jpeg';
+import Predictorimg from './assets/Math_score_predictor.jpeg';
 import Intro from './components/Intro';
 import Skills from './components/Skills';
 import Footer from './components/Footer';
@@ -29,7 +32,7 @@ const projects = [
   { 
     name: "Math Score Predictor", 
     desc: "This project predicts student exam performance (math scores) based on demographic and academic features such as gender, race/ethnicity, parental education, lunch type, test preparation course, reading score, and writing score \nThe model was trained using scikit-learn pipelines with preprocessing, and deployed with a Flask web application on Render. The application provides a UI (index.html & predict.html) where users can input student details and get predictions.", 
-    image: Math_score_predictor, 
+    image: Predictorimg, 
     tech: ['HTML', 'CSS', 'Python','numpy', 'Flask', 'scikit-learn', 'pandas','seaborn','matplotlib'],
     github: "https://github.com/sumitpatel-2322/Student_perfomance_predictor",
     live: "https://math-score-predictor-frh6.onrender.com/"
@@ -37,14 +40,14 @@ const projects = [
   { 
     name: "Trading Bot", 
     desc: "A Python trading bot I built during my learning journey! 🚀\n This is my latest project - a trading bot that connects to Binance Futures Testnet. It was super exciting to work with APIs and create both a command-line tool and a web interface!", 
-    image: Trading_bot, 
+    image: Tradingbotimg, 
     tech: ['streamlit', 'Python', 'python-binance', 'plotly', 'python-dotenv','Binance-API'],
     github: "https://github.com/sumitpatel-2322/Trading_Bot"
   },
   { 
     name: "Trading Behaviour", 
     desc: "This project analyzes the relationship between cryptocurrency trader performance and market sentiment using machine learning clustering techniques. I worked with over 211,000 trading transactions from Hyperliquid exchange and Bitcoin Fear & Greed Index data to find patterns that could help understand different types of traders", 
-    image: Cluster_bar_graph, 
+    image: Clusterimg, 
     tech: ['Python','numpy', 'Pandas', 'plotly', 'seaborn','scikit-learn','KMeans clustering'
     ],
     github: "https://github.com/sumitpatel-2322/Trading_behaviour"
