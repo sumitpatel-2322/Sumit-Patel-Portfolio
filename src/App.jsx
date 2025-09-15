8dbec92fb121533d4368b301e16316baf8cfa16d
Sumit Patel <sumitpatel2907@gmail.com>
2025-09-16 01:41:02 +0530

    A very little change

--- src/App.jsx
+++ src/App.jsx
@@ -30,19 +30,19 @@ const projects = [
     live: "https://student-ranker.onrender.com/"
   },
   { 
+    name: "Trading Bot", 
+    desc: "A Python trading bot I built during my learning journey! 🚀\n This is my latest project - a trading bot that connects to Binance Futures Testnet. It was super exciting to work with APIs and create both a command-line tool and a web interface!", 
+    image: Tradingbotimg, 
+    tech: ['streamlit', 'Python', 'python-binance', 'plotly', 'python-dotenv','Binance-API'],
+    github: "https://github.com/sumitpatel-2322/Trading_Bot"
+  },
+  { 
     name: "Math Score Predictor", 
     desc: "This project predicts student exam performance (math scores) based on demographic and academic features such as gender, race/ethnicity, parental education, lunch type, test preparation course, reading score, and writing score \nThe model was trained using scikit-learn pipelines with preprocessing, and deployed with a Flask web application on Render. The application provides a UI (index.html & predict.html) where users can input student details and get predictions.", 
     image: Predictorimg, 
     tech: ['HTML', 'CSS', 'Python','numpy', 'Flask', 'scikit-learn', 'pandas','seaborn','matplotlib'],
     github: "https://github.com/sumitpatel-2322/Student_perfomance_predictor",
     live: "https://math-score-predictor-frh6.onrender.com/"
-  },
-  { 
-    name: "Trading Bot", 
-    desc: "A Python trading bot I built during my learning journey! 🚀\n This is my latest project - a trading bot that connects to Binance Futures Testnet. It was super exciting to work with APIs and create both a command-line tool and a web interface!", 
-    image: Tradingbotimg, 
-    tech: ['streamlit', 'Python', 'python-binance', 'plotly', 'python-dotenv','Binance-API'],
-    github: "https://github.com/sumitpatel-2322/Trading_Bot"
   },
   { 
     name: "Trading Behaviour", 
